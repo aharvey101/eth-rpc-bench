e3fa32ea08791b9d1e928b9f5c5958ebab6d473c
Alexander Harvey <harvey.alex01@gmail.com>
2024-05-11 18:04:20 +1000

    added contract call benchmark

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use dotenv::dotenv;
+use ethers::abi::{Abi, Address};
+use ethers::prelude::Contract;
 use ethers::providers::{Http, Middleware, Provider, ProviderError};
-use ethers::types::U64;
-use std::fmt::Error;
+use ethers::types::{H160, U64};
+use std::str::FromStr;
+use std::sync::Arc;
 use std::time::Instant;
 
 fn get_env(key: &str) -> String {
@@ -22,8 +25,25 @@ impl Env {
 }
 #[tokio::main]
 async fn main() -> Result<(), ProviderError> {
+    let uniswap_v3_pool_abi: Abi = serde_json::from_str(
+        r#"
+        [
+                {
+                    "inputs": [],
+                    "name": "factory",
+                    "outputs": [{"internalType": "address", "name": "", "type": "address"}],
+                    "stateMutability": "view",
+                    "type": "function"
+                }
+            ]
+        "#,
+    )
+    .unwrap();
+    let uniswap_v3_pool_address =
+        Address::try_from(H160::from_str("0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640").unwrap())
+            .unwrap();
     println!("Hello, world!");
-    dotenv::dotenv().ok();
+    dotenv().ok();
     let env = Env::new();
     let alchemy_url = env.alchemy_url;
     let local_node_url = env.local_node_url;
@@ -34,23 +54,54 @@ async fn main() -> Result<(), ProviderError> {
         Provider::<Http>::try_from(local_node_url).expect("Couldn't instantiate local node w3");
 
     let next_block = alchemy_w3.get_block_number().await?;
-    println!("next block {:?}", next_block.clone());
+    println!("next block {:?}", &next_block);
 
     let now = Instant::now();
-    benchmark_req(alchemy_w3, next_block.clone()).await?;
+    benchmark_req(&alchemy_w3, &next_block).await?;
     println!("Elapsed time Achemy: {:.2?}", now.elapsed());
 
     let now = Instant::now();
-    benchmark_req(local_node_w3, next_block.clone()).await?;
+    benchmark_req(&local_node_w3, &next_block).await?;
     println!("Elapsed time Local Node: {:.2?}", now.elapsed());
+
+    let now = Instant::now();
+    benchmark_contract_call(
+        alchemy_w3.clone(),
+        uniswap_v3_pool_address.clone(),
+        uniswap_v3_pool_abi.clone(),
+    )
+    .await?;
+    println!("Elapsed time Alchemy: {:?}", now.elapsed());
+
+    let now = Instant::now();
+    benchmark_contract_call(
+        local_node_w3.clone(),
+        uniswap_v3_pool_address.clone(),
+        uniswap_v3_pool_abi.clone(),
+    )
+    .await
+    .unwrap();
+    println!("Elapsed Time contract call local node: {:?}", now.elapsed());
 
     Ok(())
 }
 
-async fn benchmark_req(prov: Provider<Http>, block_number: U64) -> Result<(), ProviderError> {
+async fn benchmark_req(prov: &Provider<Http>, block_number: &U64) -> Result<(), ProviderError> {
     for i in 1..100 {
         let block_to_get = block_number.clone() - i.clone();
         prov.get_block(block_to_get).await?;
     }
+    Ok(())
+}
+
+async fn benchmark_contract_call(
+    prov: Provider<Http>,
+    swap_address: Address,
+    abi: Abi,
+) -> Result<(), ProviderError> {
+    let arked_prov = Arc::new(prov);
+    let pool = Contract::new(swap_address, abi, arked_prov.clone());
+    let factory_address = pool.connect(arked_prov.clone()).address();
+    println!("factory address: {:?}", factory_address);
     Ok(())
 }
